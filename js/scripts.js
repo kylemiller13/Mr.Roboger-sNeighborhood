ff6778398254f6b3583855b7f733f63f475d8bb1
Kyle Miller <millerkyle85@gmail.com>
2022-02-11 15:44:42 -0800

    got it to replace first & second num to a string

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,14 @@
 
 //Business logic
 
-let str = "143231";
-function beep(digit){
+let str = "18963232";
+function beep(str){
   const strSplit = str.split("");
-  for (let i = 0; i < digit.length; i++){
-    if (i === 1){
-      str.replace(i, "Beep!"); 
-    }else{
-      return str;
-    }
+  const newArray = [];
+  // const firstResult =  str.replace(1,"Beep!")
+  for (let i = 0; i < str.length; i++){
+    str[i].replace(1, "Beep!");
+    str[i].replace(2, "Bloop!");
+    str[i].replace(3 || 32, "Won't you be my neighbor!");
   }
 }
