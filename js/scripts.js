cb0f013cf47f0a8cb08594982e4d9e45e3be6cb4
Kyle Miller <millerkyle85@gmail.com>
2022-02-11 13:01:54 -0800

    stuck on Business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,14 @@
 
 //Business logic
 
-const str = "143231";
+let str = "143231";
 function beep(digit){
   const strSplit = str.split("");
-  
+  for (let i = 0; i < digit.length; i++){
+    if (i === 1){
+      str.replace(i, "Beep!"); 
+    }else{
+      return str;
+    }
+  }
 }
