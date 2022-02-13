e5405912e94dc3772c4736ddba93247e1d649838
Kyle Miller <millerkyle85@gmail.com>
2022-02-12 17:29:56 -0800

    work on UI to display the users input

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,33 @@
 
 
 //Business logic
-
 function rogers(num){
   let orginalArray = [];
-  for (let i = 0; i <= num; i++){
-    orginalArray.push(i);
-  };
-  const newArray = [];
-  orginalArray.forEach(function(num) {
-    if (num.toString().includes(3)){
-      newArray.push("Won't you be my neighbor?");
-    }else if (num.toString().includes(2)) {
-      newArray.push("Bloop!");
-    } else if (num.toString().includes(1)){
-      newArray.push("Beep!")
-    }else{
-      newArray.push(num);
-    }
-  });
-  return newArray;
+    for (let i = 0; i <= num; i++){
+      orginalArray.push(i);
+    };
+    const newArray = [];
+    orginalArray.forEach(function(num) {
+      if (num.toString().includes(3)){
+        newArray.push("Won't you be my neighbor?");
+      }else if (num.toString().includes(2)) {
+        newArray.push("Bloop!");
+      } else if (num.toString().includes(1)){
+        newArray.push("Beep!")
+      }else{
+        newArray.push(num);
+      }
+    });
+    let combineStr = newArray.join(", ");
+    return combineStr;
+
 }
+// User interface Logic:
+$(document).ready(function() {
+  $("userInput#number").submit(function(event){
+    event.preventDefault();
+    let usersInput = parseInt($("userInput#number").val());
+    $("#result").show();
+    rogers(usersInput);
+  });
+});
