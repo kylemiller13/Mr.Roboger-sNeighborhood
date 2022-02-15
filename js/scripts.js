a3687de72c5bd21ae698fc55229d8d0a1c85580a
Kyle Miller <millerkyle85@gmail.com>
2022-02-15 00:53:02 -0800

    UI works

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 //Business logic
-function rogers(num){
+function rogers(num) {
   let orginalArray = [];
-    for (let i = 0; i <= num; i++){
-      orginalArray.push(i);
-    };
-    const newArray = [];
-    orginalArray.forEach(function(num) {
-      if (num.toString().includes(3)){
-        newArray.push("Won't you be my neighbor?");
-      }else if (num.toString().includes(2)) {
-        newArray.push("Bloop!");
-      } else if (num.toString().includes(1)){
-        newArray.push("Beep!")
-      }else{
-        newArray.push(num);
-      }
-    });
-    let combineStr = newArray.join(", ");
-    return combineStr;
+  for (let i = 0; i <= num; i++) {
+    orginalArray.push(i);
+  }
+  const newArray = [];
+  orginalArray.forEach(function (num) {
+    if (num.toString().includes(3)) {
+      newArray.push("Won't you be my neighbor?");
+    } else if (num.toString().includes(2)) {
+      newArray.push("Bloop!");
+    } else if (num.toString().includes(1)) {
+      newArray.push("Beep!");
+    } else {
+      newArray.push(num);
+    }
+  });
+  let combineStr = newArray.join(", ");
+  return combineStr;
 }
 // User interface Logic:
-$(document).ready(function() {
-  $(".userInput").submit(function(event){
+$(document).ready(function () {
+  $("#btn").click(function (event) {
     event.preventDefault();
-    let usersInput = parseInt($(".userInput#number").val());
+    let usersInput = parseInt($("#number").val());
     $("#result").text(rogers(usersInput));
   });
 });
